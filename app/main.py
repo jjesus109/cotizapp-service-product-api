fa0ecb3a7cad8f7980d18115c514f118cfbc7ad8
jesus <jesjalher@gmail.com>
2023-05-24 23:00:21 -0600

    feat: :sparkles: Add blueprint for the rest of endpoints

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from app.utilities import get_token
+from app.models import ServiceModel, ServiceUpdateModel
 
 from fastapi import FastAPI
 import requests
@@ -16,12 +17,25 @@ async def search_product(category: str, brand: str, word_to_search: str):
     autorization_header = {
         "Authorization": f"Bearer {token}"
     }
-    requests.get(URL, headers=autorization_header)
+    response = requests.get(URL, headers=autorization_header)
+    return response
 
 
-@app.get("/")
-async def get_product():
-    return "Hi, man"
+@app.get("/api/v1/services")
+async def search_service(service: str):
+    pass
+
+
+@app.post("/api/v1/services")
+async def create_service(service: ServiceModel):
+    pass
+
+
+# reference:
+# https://fastapi.tiangolo.com/tutorial/body-updates/#using-pydantics-exclude_unset-parameter
+@app.patch("/api/v1/services/{service_id}")
+async def modify_service(service_id: int, service: ServiceUpdateModel):
+    pass
 
 
 if __name__ == "__main__":
--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from typing import Union
 from pydantic import BaseModel
 
 
@@ -5,3 +6,19 @@ class SearchParams(BaseModel):
     category: str
     brand: str
     word_to_search: str
+
+
+class ServiceModel(BaseModel):
+    name: str
+    description: str
+    client_price: float
+    real_price: float
+    image: str
+
+
+class ServiceUpdateModel(BaseModel):
+    name: Union[str, None] = None
+    description: Union[str, None] = None
+    client_price: Union[float, None] = None
+    real_price: Union[float, None] = None
+    image: Union[str, None] = None
