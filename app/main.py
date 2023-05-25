be6f9efcd2923892b803d38ac3f3da86a3338471
jesus <jesjalher@gmail.com>
2023-05-24 23:56:15 -0600

    feat: :sparkles: Add create service func

--- app/main.py
+++ app/main.py
@@ -1,9 +1,21 @@
 from app.utilities import get_token
 from app.models import ServiceModel, ServiceUpdateModel
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, status
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
 import requests
 import uvicorn
+
+from pymongo.errors import (
+    ConnectionFailure,
+    ExecutionTimeout
+)
+import os
+import motor.motor_asyncio
+
+client = motor.motor_asyncio.AsyncIOMotorClient(os.environ["MONGODB_URL"])
+db = client.business
 
 
 app = FastAPI()
@@ -26,9 +38,23 @@ async def search_service(service: str):
     pass
 
 
-@app.post("/api/v1/services")
+@app.post(
+        "/api/v1/services",
+        response_description="Add new service",
+        response_model=ServiceModel)
 async def create_service(service: ServiceModel):
-    pass
+    service = jsonable_encoder(service)
+    try:
+        await db["service"].insert_one(service)
+    except (ConnectionFailure, ExecutionTimeout):
+        HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Could not insert data problems with DB connection"
+        )
+    return JSONResponse(
+        status_code=status.HTTP_201_CREATED,
+        content=service
+    )
 
 
 # reference:
--- app/models.py
+++ app/models.py
@@ -1,5 +1,23 @@
-from typing import Union
-from pydantic import BaseModel
+from typing import Optional
+
+from pydantic import BaseModel, Field
+from bson import ObjectId
+
+
+class PyObjectId(ObjectId):
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, v):
+        if not ObjectId.is_valid(v):
+            raise ValueError("Invalid objectid")
+        return ObjectId(v)
+
+    @classmethod
+    def __modify_schema__(cls, field_schema):
+        field_schema.update(type="string")
 
 
 class SearchParams(BaseModel):
@@ -9,16 +27,40 @@ class SearchParams(BaseModel):
 
 
 class ServiceModel(BaseModel):
-    name: str
-    description: str
-    client_price: float
-    real_price: float
-    image: str
+    id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
+    name: str = Field(...)
+    description: str = Field(...)
+    client_price: float = Field(...)
+    real_price: float = Field(...)
+
+    class Config:
+        allow_population_by_field_name = True
+        arbitrary_types_allowed = True
+        json_encoders = {ObjectId: str}
+        schema_extra = {
+            "example": {
+                "name": "Mantenimiento",
+                "description": "Mantenimiento preventivo y correctivo",
+                "client_price": 522,
+                "real_price": 200
+            }
+        }
 
 
 class ServiceUpdateModel(BaseModel):
-    name: Union[str, None] = None
-    description: Union[str, None] = None
-    client_price: Union[float, None] = None
-    real_price: Union[float, None] = None
-    image: Union[str, None] = None
+    name: Optional[str]
+    description: Optional[str]
+    client_price: Optional[float]
+    real_price: Optional[float]
+
+    class Config:
+        arbitrary_types_allowed = True
+        json_encoders = {ObjectId: str}
+        sschema_extra = {
+            "example": {
+                "name": "Mantenimiento",
+                "description": "Mantenimiento preventivo y correctivo",
+                "client_price": 600,
+                "real_price": 180
+            }
+        }
